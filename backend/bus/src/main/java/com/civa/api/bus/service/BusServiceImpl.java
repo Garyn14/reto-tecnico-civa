2a7dfcd0dccfe4eb1963dfda9d371a920a8eada3
Garyn14 <garyn.fernando.hr@gmail.com>
2024-11-14 20:33:47 -0500

    implements service

--- backend/bus/src/main/java/com/civa/api/bus/service/BusServiceImpl.java
+++ backend/bus/src/main/java/com/civa/api/bus/service/BusServiceImpl.java
@@ -1,8 +1,11 @@
 package com.civa.api.bus.service;
 
+import com.civa.api.bus.exception.BrandNotFoundException;
+import com.civa.api.bus.exception.BusNotFoundException;
 import com.civa.api.bus.mapper.BusMapper;
 import com.civa.api.bus.model.dto.BusCreateRequest;
 import com.civa.api.bus.model.dto.BusResponse;
+import com.civa.api.bus.model.entity.Bus;
 import com.civa.api.bus.repository.BrandRepository;
 import com.civa.api.bus.repository.BusRepository;
 import lombok.RequiredArgsConstructor;
@@ -20,26 +23,56 @@ public class BusServiceImpl implements BusService{
 
     @Override
     public BusResponse findById(Long id) {
-        return null;
+        return busRepository.findById(id)
+                .map(busMapper::toBusResponse)
+                .orElseThrow(BusNotFoundException::new);
     }
 
     @Override
     public List<BusResponse> findAll() {
-        return null;
+        return busRepository.findAll()
+                .stream()
+                .map(busMapper::toBusResponse)
+                .toList();
     }
 
     @Override
     public BusResponse save(BusCreateRequest request) {
-        return null;
+        return brandRepository.findById(request.getBrandId())
+                .map(brand -> {
+                    Bus bus = new Bus();
+                    bus.setBusNumber(request.getBusNumber());
+                    bus.setLicensePlate(request.getLicensePlate());
+                    bus.setFeatures(request.getFeatures());
+                    bus.setIsActive(request.getIsActive());
+                    bus.setBrand(brand);
+                    return busRepository.save(bus);
+                })
+                .map(busMapper::toBusResponse)
+                .orElseThrow(BrandNotFoundException::new);
     }
 
     @Override
     public BusResponse update(Long id, BusCreateRequest request) {
-        return null;
+        return busRepository.findById(id)
+                .map(bus -> brandRepository.findById(request.getBrandId())
+                        .map(brand -> {
+                            bus.setBusNumber(request.getBusNumber());
+                            bus.setLicensePlate(request.getLicensePlate());
+                            bus.setFeatures(request.getFeatures());
+                            bus.setIsActive(request.getIsActive());
+                            bus.setBrand(brand);
+                            return busRepository.save(bus);
+                        })
+                        .orElseThrow(BrandNotFoundException::new)
+                )
+                .map(busMapper::toBusResponse)
+                .orElseThrow(BusNotFoundException::new);
     }
 
     @Override
     public void delete(Long id) {
-
+        if (busRepository.findById(id).isEmpty()) throw new BusNotFoundException();
+        busRepository.deleteById(id);
     }
 }
